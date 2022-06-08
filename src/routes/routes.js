bc4dbfbdd60afa9ac98f3082f0b9ebce95d1339e
Param <user@users-MacBook-Pro.local>
2022-06-07 20:06:38 -0700

    added MVC architecture

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,14 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = require("express");
 var Controllers = require("../controllers/controllers");
 var stocksRouter = express_1.Router();
-// stocksRouter.get('/stocks',async (req,res) => {
-//     const products = await productRepository.find({
-//         where:{
-//             id: 2
-//         }
-//     });
-//     res.status(200).json(products);
-// });    
 //making a stock entry for a company
 stocksRouter.post('/stocks', Controllers.RegisterStock);
 //sending the company stocks back
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,15 +3,6 @@ import * as Controllers from '../controllers/controllers';
 
 const stocksRouter = Router();
 
-// stocksRouter.get('/stocks',async (req,res) => {
-//     const products = await productRepository.find({
-//         where:{
-//             id: 2
-//         }
-//     });
-//     res.status(200).json(products);
-// });    
-
 //making a stock entry for a company
 stocksRouter.post('/stocks',Controllers.RegisterStock);    
 
